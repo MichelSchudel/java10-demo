9a473541738853a8db9e4376ad4a22a52930b325
Michel Schudel <michel.schudel@gmail.com>
2018-02-14 14:06:44 +0100

    Refactoring

---
+++ src/nl/craftsmen/java10demo/Java10Demonstrator.java
@@ -0,0 +1,47 @@
+package nl.craftsmen.java10demo;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class Java10Demonstrator {
+
+    public static void demonstrate(List<String> stringList, Map<String,String> stringMap, Set<Integer> intSet) {
+        System.out.println("Java 10 result:");
+        simpleExamples();
+        printLoops(stringList);
+        collectionsDemo(stringList, stringMap, intSet);
+    }
+
+    private static void simpleExamples() {
+        var text = "Hello, Java 10!";
+        System.out.println(text);
+        var textFromMethod = getName();
+        System.out.println(textFromMethod);
+    }
+
+    private static void printLoops(List<String> stringList) {
+        for (var element: stringList) {
+            System.out.println(element);
+        }
+        for (var i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+
+    private static void collectionsDemo(List<String> stringList, Map<String, String> stringMap, Set<Integer> intSet) {
+        var stringListCopy = List.copyOf(stringList);
+        var stringMapCopy = Map.copyOf(stringMap);
+        var intSetCopy = Set.copyOf(intSet);
+        System.out.println(stringListCopy);
+        System.out.println(stringMapCopy);
+        System.out.println(intSetCopy);
+        var unmodifiableIntSet = intSet.stream().collect(Collectors.toUnmodifiableSet());
+        System.out.println(unmodifiableIntSet);
+    }
+
+    private static String getName() {
+        return "Name";
+    }
+}
---
+++ src/nl/craftsmen/java10demo/Java9Demonstrator.java
@@ -0,0 +1,48 @@
+package nl.craftsmen.java10demo;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class Java9Demonstrator {
+
+    public static void demonstrate(List<String> stringList, Map<String,String> stringMap, Set<Integer> intSet) {
+        System.out.println("Java 9 result:");
+        simpleExamples();
+        printLoops(stringList);
+        collectionsDemo(stringList, stringMap, intSet);
+    }
+
+    private static void simpleExamples() {
+        String text = "Hello, Java 9!";
+        System.out.println(text);
+        String textFromMethod = getName();
+        System.out.println(textFromMethod);
+    }
+
+    private static void printLoops(List<String> stringList) {
+        for (String element: stringList) {
+            System.out.println(element);
+        }
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+
+    private static void collectionsDemo(List<String> stringList, Map<String, String> stringMap, Set<Integer> intSet) {
+        List<String> stringListCopy = Collections.unmodifiableList(stringList);
+        Map<String, String> stringMapCopy = Collections.unmodifiableMap(stringMap);
+        Set<Integer> intSetCopy = Collections.unmodifiableSet(intSet);
+        System.out.println(stringListCopy);
+        System.out.println(stringMapCopy);
+        System.out.println(intSetCopy);
+        Set<Integer> unmodifiableIntSet = Collections.unmodifiableSet(intSet.stream().collect(Collectors.toSet()));
+        System.out.println(unmodifiableIntSet);
+    }
+
+    private static String getName() {
+        return "Name";
+    }
+}
